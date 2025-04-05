dd469f3bdc45f013cc443440c62f2a04f753557a
Xing Xing <xxin7882@uni.sydney.edu.au>
2025-04-05 13:57:54 +1100

    implement id.py

--- pangu/particle/text/id.py
+++ pangu/particle/text/id.py
@@ -16,9 +16,39 @@ class Id:
         """
         return str(uuid.uuid4())[:length]
     
+    @staticmethod
+    def random(
+        length: int = 8, 
+        include_letters = True, 
+        include_numbers = False, 
+        include_upper = False, 
+        include_lower = True,
+    ) -> str:
+        """
+        Generate a random string of the specified length.
+        
+        """
+        if include_letters:
+            if include_upper and include_lower:
+                alphabet = string.ascii_letters
+            elif include_upper:
+                alphabet = string.ascii_uppercase
+            elif include_lower:
+                alphabet = string.ascii_lowercase
+            else:
+                alphabet = ''
+        else:
+            alphabet = ''
+        
+        if include_numbers:
+            alphabet += string.digits
+        
+        return ''.join(random.choice(alphabet) for _ in range(length))
+    
 
-    def __init__(self, id: str = ''):
-        self.id = id or self.uuid()
+    
+        
+
 
 
     
